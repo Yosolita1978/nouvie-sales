6948b572f87aca61015f23e1a1317d1ef18e84a2
Cristina Rodriguez <yosola@gmail.com>
2026-04-03 17:05:12 -0700

    Design layouts for all platforms

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon-96x96.png" type="image/png" sizes="96x96" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
